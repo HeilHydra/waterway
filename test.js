a8ff776e332eacdf0ea13b83259c95abcec205fd
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-03-18 16:06:25 +0000

    Refactor connections, complete streams

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 var Waterway = require("./lib/Waterway");
 
+
 var waterway1 = new Waterway();
-waterway1.request("bar")
-  .respond(function (data) {
+waterway1.stream("bar")
+  .on("data", function (data) {
     console.log("RECEIVED", data);
-    return "YOLO" + JSON.stringify(data);
   });
 
 var waterway2 = new Waterway();
-waterway2.request("bar")
-  .send({ yolo: "swaghetti" })
-  .then(function (response) {
-    console.log("RESPONSE", response);
-  })
+waterway2.stream("bar")
+  .write({ sup: "hello" });
