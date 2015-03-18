ed772e4912d294b1cf695042129afd6ad8a69575
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-03-18 13:08:13 +0000

    Make requests work

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 var Waterway = require("./lib/Waterway");
 
 var waterway1 = new Waterway();
-waterway1.event("*", "bar").on(function (data) {
-  console.log("RECEIVED");
-});
+waterway1.request("bar")
+  .respond(function (data) {
+    console.log("RECEIVED", data);
+    return "YOLO" + JSON.stringify(data);
+  });
 
 var waterway2 = new Waterway();
-waterway2.event("foo", "bar").emit({ yolo: "swaghetti" });
+waterway2.request("bar")
+  .send({ yolo: "swaghetti" })
+  .then(function (response) {
+    console.log("RESPONSE", response);
+  })
